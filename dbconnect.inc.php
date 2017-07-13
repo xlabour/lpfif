b0356879f9978f27a9a6f68f3da94b7f71f2c27e
XLabour <syarif.hidayatullah@gmail.com>
2017-07-13 09:56:42 +0700

    Update dbconnect.inc.php

--- dbconnect.inc.php
+++ dbconnect.inc.php
@@ -1,5 +1,5 @@
 <?php
-echo "a".$DB_HOST.$DB_USERNAME;
+//echo "a".$DB_HOST.$DB_USERNAME;
 $dblink = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD) or die('Could not connect: ' . mysqli_error($dblink));
 mysqli_select_db($dblink, $DB_NAME) or die(mysqli_error($dblink));
 ?>
